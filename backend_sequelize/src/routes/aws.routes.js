1e7a5d4f34a76682f27b57511458af2b2ace1ef8
ElliottWong <ELLIOTTWONG.20@ichat.sp.edu.sg>
2021-11-08 11:48:53 +0800

    Removed AWS Update Route
    Forgot to remove the route from AWS routes.js

--- backend_sequelize/src/routes/aws.routes.js
+++ backend_sequelize/src/routes/aws.routes.js
@@ -7,8 +7,6 @@ router.get('/getCustomer/:customerId', awsController.processGetOneCustomer);
 
 router.post('/addCustomer', awsController.processAddCustomer);
 
-router.put('/updateCustomer/:customerId', awsController.processUpdateCustomer);
-
 router.delete('/deleteCustomer/:customerId', awsController.processDeleteCustomer);
 
 module.exports = router;
