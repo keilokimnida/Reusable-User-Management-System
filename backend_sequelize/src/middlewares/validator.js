9180471cd288356dc8ee21febf6363cbffafc475
ElliottWong <ELLIOTTWONG.20@ichat.sp.edu.sg>
2021-12-02 23:10:07 +0800

    Update validator.js
    Pushing this to clear, will fix this later

--- backend_sequelize/src/middlewares/validator.js
+++ backend_sequelize/src/middlewares/validator.js
@@ -1,9 +1,9 @@
 const validator = require('validator');
 
 module.exports.validateText = (input) => {
-    if (validators.isEmpty(input))
+    if (validator.isEmpty(input))
         throw new Error('Missing Field');
-    if (validators.isAlpha(input))
+    if (validator.isAlpha(input))
         throw new Error('Input is not alphebetic');
 
     input = validator.escape(input);
